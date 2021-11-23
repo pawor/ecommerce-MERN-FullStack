156b0ca98c27f2dd414e10b3db92858979d7ca97
pawor <pawor.emi@gmail.com>
2021-11-23 12:49:40 +0700

    สร้าง category ในส่วน backend  -close #9

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(fileupload({
 
 //Routes
 app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/categoryRouter'))
 
 //Connect to MongoDB
 const URI = process.env.MONGODB_URL
