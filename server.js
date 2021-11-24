42d9d01db00ddf6e0ba14a988381899846d69533
pawor <pawor.emi@gmail.com>
2021-11-24 11:07:31 +0700

    สร้าง Upload and Delete Image on Cloudinary -close #10

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(fileupload({
 //Routes
 app.use('/user', require('./routes/userRouter'))
 app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
 
 //Connect to MongoDB
 const URI = process.env.MONGODB_URL
